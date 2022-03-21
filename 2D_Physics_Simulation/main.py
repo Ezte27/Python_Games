76d202e1e9e89c361503bea33ce5da75d785de96
Esteban <estedcg27@gmail.com>
2022-03-20 21:49:50 -0400

    updated file via upload

--- 2D_Physics_Simulation/main.py
+++ 2D_Physics_Simulation/main.py
@@ -7,16 +7,61 @@ pygame.init()
 WIDTH, HEIGHT = 1000, 800
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def draw(space, window, draw_options):
+def draw(space, window, draw_options, line):
     window.fill('white')
+    if line:
+        pygame.draw.line(window, 'black', line[0], line[1])
     space.debug_draw(draw_options)
     pygame.display.update()
 
-def create_circle(space, radius, mass):
-    body = pymunk.Body()
-    body.position = (WIDTH/2, HEIGHT/2)
+def calculate_distance(p1, p2):
+    return math.sqrt((p2[1] - p1[1])**2 + (p2[0] - p1[0])**2)
+
+def calculate_angle(p1, p2):
+    return math.atan2(p2[1] - p1[1], p2[0] - p1[0])
+
+def create_boundaries(space, width, height):
+    rects = [
+        [(width/2, height - 10), (width, 20)],
+        [(width/2, 10), (width, 20)],
+        [(10, height/2), (20, height)],
+        [(width - 10, height/2), (20, height)],
+    ]
+
+    for pos, size in rects:
+        body = pymunk.Body(body_type = pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        shape.elasticity = 0.4
+        shape.friction = 0.5
+        shape.color = (20, 20, 20, 97)
+        space.add(body, shape)
+
+def create_structures(space, width, height):
+    BROWN = (139, 69, 19, 100)
+    rects = [
+        [(600, height - 120), (40, 200), BROWN, 100],
+        [(900, height - 120), (40, 200), BROWN, 100],
+        [(750, height - 240), (340, 40), BROWN, 150],
+    ]
+
+    for pos, size, color, mass in rects:
+        body = pymunk.Body(body_type = pymunk.Body.DYNAMIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        shape.elasticity = 0.2
+        shape.friction = 0.5
+        shape.mass = mass
+        shape.color = (20, 20, 20, 97)
+        space.add(body, shape)
+
+def create_circle(space, radius, mass, pos):
+    body = pymunk.Body(body_type = pymunk.Body.STATIC)
+    body.position = (pos)
     shape = pymunk.Circle(body, radius)
     shape.mass = mass
+    shape.elasticity = 0.7
+    shape.friction = 0.3
     shape.color = (255, 0, 0, 100)
     space.add(body, shape)
     return shape
@@ -30,17 +75,40 @@ def run(window, width, height):
     space = pymunk.Space()
     space.gravity = (0, 981)
 
-    ball = create_circle(space, 30, 10)
+    create_boundaries(space, width, height)
 
     draw_options = pymunk.pygame_util.DrawOptions(window)
 
+    pressed_pos = None
+    ball = None
+
     while run:
+        line = None
+        if ball and pressed_pos:
+            line = [pressed_pos, pygame.mouse.get_pos()]
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if not ball:
+                    pressed_pos = pygame.mouse.get_pos()
+                    ball = create_circle(space, 30, 10, pressed_pos)
+                elif pressed_pos:
+                    ball.body.body_type = pymunk.Body.DYNAMIC
+                    angle = calculate_angle(*line) # * asterisk breaks the list in the different elements
+                    force = calculate_distance(*line) * 50
+                    fx = math.cos(angle) * force
+                    fy = math.sin(angle) * force
+                    ball.body.apply_impulse_at_local_point((fx, fy), (0, 0))
+                    pressed_pos = None
+                else:
+                    space.remove(ball, ball.body)
+                    ball = None
+                
 
-        draw(space, window, draw_options)
+        draw(space, window, draw_options, line)
 
         space.step(dt)
         clock.tick(fps)
