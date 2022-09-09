8373c69c1bc79deaefb79e16a3db42b75629fc28
Esteban <estedcg27@gmail.com>
2022-09-08 22:57:21 -0400

    added day night cycle without having to go to bed

--- Pydew_Valley/code/sky.py
+++ Pydew_Valley/code/sky.py
@@ -3,6 +3,7 @@ import pygame
 from settings import *
 from support import import_folder
 from sprites import Generic
+from timer import Timer
 
 class Rain:
     def __init__(self, all_sprites) -> None:
@@ -61,13 +62,47 @@ class Sky:
     def __init__(self) -> None:
         self.display_surface = pygame.display.get_surface()
         self.full_surf = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        
         self.start_color = [255, 255, 255]
         self.end_color = (20, 25, 60)
 
+        self.color_index = [0, 0, 0]
+
+        self.sunrise = False
+        self.sunset = True
+
+        # Timers
+        self.timer = Timer((1/DAYTIME_SPEED) * DAY_NIGHT_DURATION)
+
     def display(self, dt):
-        for index, value in enumerate(self.end_color):
-            if self.start_color[index] > value:
-                self.start_color[index] -= DAYTIME_SPEED * dt
+        self.timer.update()
+
+        if not self.timer.active:
+            if self.sunset:
+                for index, value in enumerate(self.end_color):
+                    if self.start_color[index] > value:
+                        self.start_color[index] -= DAYTIME_SPEED * dt
+                    else:
+                        self.color_index[index] = 1
+                    
+                if all(self.color_index):
+                    self.sunrise = True
+                    self.sunset  = False
+                    self.timer.activate()
+                    print("timer")
+            
+            elif self.sunrise:
+                for index, value in enumerate(self.end_color):
+                    if self.start_color[index] < 255:
+                        self.start_color[index] += DAYTIME_SPEED * dt
+                    else:
+                        self.color_index[index] = 0
+
+                if not(all(self.color_index)):
+                    self.sunrise = False
+                    self.sunset  = True
+                    self.timer.activate()
+                    print("timer")
 
         self.full_surf.fill(self.start_color)
         self.display_surface.blit(self.full_surf, (0,0), special_flags= pygame.BLEND_RGBA_MULT) # dont show white FLAG
