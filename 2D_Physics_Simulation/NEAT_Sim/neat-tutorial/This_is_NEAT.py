507559827c5aa037c5549f09287a46abfd8cc588
Esteban <estedcg27@gmail.com>
2022-09-13 16:23:15 -0400

    added new tutorial code from sentdex

---
+++ 2D_Physics_Simulation/NEAT_Sim/neat-tutorial/This_is_NEAT.py
@@ -0,0 +1,87 @@
+"""
+Single-pole balancing experiment using a feed-forward neural network.
+"""
+
+import multiprocessing
+import os
+import pickle
+
+import neat
+import neuralnet_visualize as visualize
+import numpy as np
+
+import gym
+
+runs_per_net = 2
+
+
+# Use the NN network phenotype and the discrete actuator force function.
+def eval_genome(genome, config):
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+
+    fitnesses = []
+
+    for runs in range(runs_per_net):
+        env = gym.make("CartPole-v1")
+
+        observation, info = env.reset()
+
+        # Run the given simulation for up to num_steps time steps.
+        fitness = 0.0
+        done = False
+        while not done:
+    
+            action = np.argmax(net.activate(observation))
+
+            observation, reward, done, truncated, info = env.step(action)
+
+            fitness += reward
+
+        fitnesses.append(fitness)
+
+    # The genome's fitness is its worst performance across all runs.
+    return np.mean(fitnesses)
+
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = eval_genome(genome, config)
+
+
+def run():
+    # Load the config file, which is assumed to live in
+    # the same directory as this script.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    pop = neat.Population(config)
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.add_reporter(neat.StdOutReporter(True))
+
+    pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), eval_genome)
+    winner = pop.run(pe.evaluate)
+
+    # Save the winner.
+    with open('winner', 'wb') as f:
+        pickle.dump(winner, f)
+
+    print(winner)
+
+    visualize.plot_stats(stats, ylog=True, view=True, filename="feedforward-fitness.svg")
+    visualize.plot_species(stats, view=True, filename="feedforward-speciation.svg")
+
+    node_names = {-1: 'x', -2: 'dx', -3: 'theta', -4: 'dtheta', 0: 'control'}
+    visualize.draw_net(config, winner, True, node_names=node_names)
+
+    visualize.draw_net(config, winner, view=True, node_names=node_names,
+                       filename="winner-feedforward.gv")
+    visualize.draw_net(config, winner, view=True, node_names=node_names,
+                       filename="winner-feedforward-enabled-pruned.gv", prune_unused=True)
+
+
+if __name__ == '__main__':
+    run()
