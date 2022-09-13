0ba06765052d34d77794484a23524c8e78677fbf
Esteban <estedcg27@gmail.com>
2022-09-13 00:04:14 -0400

    FINALLY!! fixed the bugs and errors (It WORKS!)

--- Simple_NEAT_AI/main.py
+++ Simple_NEAT_AI/main.py
@@ -4,7 +4,6 @@
 
 import math
 import sys
-from tabnanny import check
 import pygame, neat, pickle
 from random import randint, choice
 from settings import *
@@ -36,34 +35,11 @@ def eval_genomes(genomes, config):
     run(nets, ge, players)
 
 def run(nets, ge, players):
-    bg_color = choice(BG_COLORS)
-    print(f"\n BACKGROUND COLOR: {bg_color}\n")
-    
-    start_time = 0
+    bg_color = get_random_color()
     runs = 0
-
-    running = True
-    while running:
-
-        clock.tick(FPS)
-        screen.fill(bg_color)
-
-        # Check for pygame events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                sys.exit()
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
-                    running = False
-
-        if pygame.time.get_ticks() - start_time >= COLOR_DURATION: # Checks for new round
-            start_time = pygame.time.get_ticks()
-            if runs >= COLOR_CHANGES_NUM:
-                running = False
-
+    
+    if FAST_AI_TRAINING:
+        while runs < FAST_COLOR_CHANGES_NUM:
             runs += 1
 
             bg_color = get_random_color()
@@ -75,8 +51,9 @@ def run(nets, ge, players):
             # Get AI Output
             for i, net in enumerate(nets):
                 output = nets[i].activate(get_inputs(bg_color))
-                # print(output) # DEBUG
+                #print(f"\n UNCHANGED: {output}") # DEBUG
                 output = transform_output(output)
+                #print(output)
                 players[i].color = output
 
             # Update Fitness
@@ -85,22 +62,147 @@ def run(nets, ge, players):
                     genome.fitness += 10
                 else:
                     genome.fitness -= 6
-        
-        # Drawing
-        for player in players:
-            pygame.draw.rect(screen, player.color, player.rect)
+    
+    else:
+        start_time = 0
 
-        gen_text = font.render(f"Generation: {generation}", False, 'Black')
+        running = True
+
+        while running:
+
+            clock.tick(FPS)
+
+            # Check for pygame events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+                    sys.exit()
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        running = False
+
+            if pygame.time.get_ticks() - start_time >= COLOR_DURATION: # Checks for new round
+                    
+                start_time = pygame.time.get_ticks()
+                if runs >= COLOR_CHANGES_NUM:
+                    running = False
+
+                runs += 1
+
+                bg_color = get_random_color()
+
+                # Reset the colors of the players
+                for player in players:
+                    player.reset_color()
+
+                # Get AI Output
+                for i, net in enumerate(nets):
+                    output = nets[i].activate(get_inputs(bg_color))
+                    #print(f"\n UNCHANGED: {output}") # DEBUG
+                    output = transform_output(output)
+                    #print(output)
+                    players[i].color = output
+
+                # Update Fitness
+                for i, genome in enumerate(ge):
+                    if check_color(players[i], bg_color):
+                        genome.fitness += 10
+                    else:
+                        genome.fitness -= 6
+            
+            # Drawing
+            screen.fill(bg_color)
+
+            for player in players:
+                pygame.draw.rect(screen, player.color, player.rect)
+
+            gen_text = font.render(f"Generation: {generation}", False, 'Black')
+            gen_text_rect = gen_text.get_rect(topleft = (0, 0))
+            screen.blit(gen_text, gen_text_rect)
+
+            try:
+                best_genome_fitness_text = font.render(f"Average fitness: {round(stats.get_fitness_mean()[generation - 2], 2)}", False, 'Black')
+                best_genome_fitness_text_rect = best_genome_fitness_text.get_rect(topright = (SCREEN_WIDTH, 0))
+                screen.blit(best_genome_fitness_text, best_genome_fitness_text_rect)
+            
+            except:
+                pass
+            
+            pygame.display.update()
+
+def test_ai(genome, config):
+
+    # NEAT Setup
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+    player = Player((SCREEN_WIDTH//2 - PLAYER_WIDTH, SCREEN_HEIGHT//2 - PLAYER_HEIGHT))
+
+    bg_color = (255, 255, 255)
+    accuracy = 0
+    total = 0
+    correct = 0
+
+    running = True
+    while running:
+        
+        clock.tick(FPS)
+        screen.fill(bg_color)
+
+        # Check for pygame events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+            # Keyboard Input
+        # keys = pygame.key.get_pressed()
+        # if True:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    bg_color = get_random_color()
+                    total += 1
+
+                    # AI OUTPUT
+                    output = net.activate(get_inputs(bg_color))
+                    player.color = transform_output(output)
+
+                    # Updating accuracy
+                    if check_color(player, bg_color):
+                        correct += 1  
+                    else:
+                        print(output) # DEBUG
+                    accuracy = calculate_accuracy(correct, total)
+                
+                elif event.key == pygame.K_e:
+                    for i in range(50000):
+                        bg_color = get_random_color()
+                        total += 1
+
+                        # AI OUTPUT
+                        output = net.activate(get_inputs(bg_color))
+                        player.color = transform_output(output)
+
+                        # Updating accuracy
+                        if check_color(player, bg_color):
+                            correct += 1  
+                        accuracy = calculate_accuracy(correct, total)
+
+        # Drawing
+        pygame.draw.rect(screen, player.color, player.rect)
+
+        gen_text = font.render(f"Accuracy: {round(accuracy, 2)}%", False, 'Black', 'White')
         gen_text_rect = gen_text.get_rect(topleft = (0, 0))
         screen.blit(gen_text, gen_text_rect)
-        try:
-            best_genome_fitness_text = font.render(f"Best fitness: {stats.best_genome().fitness}", False, 'Black')
-            best_genome_fitness_text_rect = best_genome_fitness_text.get_rect(topright = (SCREEN_WIDTH, 0))
-            screen.blit(best_genome_fitness_text, best_genome_fitness_text_rect)
-        except:
-            pass
 
         pygame.display.update()
+        
+def calculate_accuracy(correct, total):
+    if total != 0:
+        return (correct / total) * 100
+    else:
+        return 0
 
 def check_color(player, bg_color):
     if player.color == bg_color:
@@ -111,11 +213,11 @@ def get_inputs(bg_color): # HAHAHA what a useless function!
     return bg_color
 
 def transform_output(output):
-    transformed_output = tuple(round(elem * 255, 0) for elem in output)
+    transformed_output = tuple(round(elem * 255) for elem in output)
     return transformed_output
 
 def get_random_color():
-    return tuple(randint(0, 255) for i in range(0, 3))#choice(BG_COLORS)
+    return choice(BG_COLORS)#tuple(randint(0, 255) for i in range(0, 3))#
 
 if __name__ == '__main__':
     # Pygame Setup
@@ -131,20 +233,28 @@ if __name__ == '__main__':
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
                                 neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
     
-    # Create core evolution class
-    p = neat.Population(config)
-    print(p.generation)
-
-    # Add Reporter
-    p.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    p.add_reporter(stats)
-
-    # Run NEAT
-    generation = 0
-
-    winner = p.run(eval_genomes, 5)#MAX_GENERATIONS)
-    # Save the winner
     best_genome_path = f"{os.getcwd()}/best_genome.pickle"
-    with open(best_genome_path, 'wb') as f:
-        pickle.dump(winner, f)
+
+    if input("Test or Train AI [0 / 1]") == '1':
+        # Create core evolution class
+        p = neat.Population(config)
+        print(p.generation)
+
+        # Add Reporter
+        p.add_reporter(neat.StdOutReporter(True))
+        stats = neat.StatisticsReporter()
+        p.add_reporter(stats)
+
+        # Run NEAT
+        generation = 0
+
+        winner = p.run(eval_genomes, MAX_GENERATIONS)
+
+        # Save the winner
+        with open(best_genome_path, 'wb') as f:
+            pickle.dump(winner, f)
+    
+    else:
+        with open(best_genome_path, 'rb') as f:
+            genome = pickle.load(f)
+        test_ai(genome, config)
--- Simple_NEAT_AI/settings.py
+++ Simple_NEAT_AI/settings.py
@@ -5,7 +5,7 @@ SCREEN_CAPTION = "I dont know what to name this program"
 FPS = 120
 
 CONFIG_PATH = f"{os.getcwd()}/config.txt"
-MAX_GENERATIONS = 600
+MAX_GENERATIONS = 30
 GENERATION_TIME = 7000 # In milliseconds
 
 PLAYER_WIDTH = 30
@@ -17,7 +17,11 @@ PREY_HEIGHT = 40
 PREY_COLORS = ['Red']
 
 #               -Red        - Yellow       - Green       - Cyan        - Blue       - Purple
-BG_COLORS = [(200, 0, 0), (200, 200, 0), (0, 200, 0), (0, 200, 200), (0, 0, 200), (160, 32, 240)]
-BG_COLORS.extend([(255,0,255), (192,192,192), (128,128,128), (128,0,0), (128,128,0), (0,128,0), (165,42,42), (255,99,71), (255,165,0), (0,139,139), (0,206,209), ])
-COLOR_CHANGES_NUM = 3
-COLOR_DURATION = 3500
+#BG_COLORS = [(200, 0, 0), (200, 200, 0), (0, 200, 0), (0, 200, 200), (0, 0, 200), (160, 32, 240)]
+#BG_COLORS.extend([(255,0,255), (192,192,192), (128,128,128), (128,0,0), (128,128,0), (0,128,0), (165,42,42), (255,99,71), (255,165,0), (0,139,139), (0,206,209), ])
+COLOR_CHANGES_NUM = 6
+COLOR_DURATION = 500
+BG_COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 255), (255, 255, 0), (0, 255, 255)]
+
+FAST_AI_TRAINING = True
+FAST_COLOR_CHANGES_NUM = 500
