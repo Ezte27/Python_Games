276205013d7cf5ec18cb517f3e79fe54c4f54ddb
Esteban <estedcg27@gmail.com>
2022-09-13 17:26:21 -0400

    ya?

--- 2D_Physics_Simulation/NEAT_Sim/neat-tutorial/This_is_NEAT.py
+++ 2D_Physics_Simulation/NEAT_Sim/neat-tutorial/This_is_NEAT.py
@@ -71,16 +71,15 @@ def run():
 
     print(winner)
 
-    visualize.plot_stats(stats, ylog=True, view=True, filename="feedforward-fitness.svg")
-    #visualize.plot_species(stats, view=True, filename="feedforward-speciation.svg")
+    visualize.plot_stats(stats, ylog=True, view=True, filename=os.path.join(local_dir, "feedforward-fitness.svg"))
+    visualize.plot_species(stats, view=True, filename=os.path.join(local_dir, "feedforward-speciation.svg"))
 
-    # node_names = {-1: 'x', -2: 'dx', -3: 'theta', -4: 'dtheta', 0: 'control'}
+    node_names = {-1: 'x', -2: 'dx', -3: 'theta', -4: 'dtheta', 0: 'control'}
+    
     # visualize.draw_net(config, winner, True, node_names=node_names)
 
-    # visualize.draw_net(config, winner, view=True, node_names=node_names,
-    #                    filename=os.path.join(local_dir, "winner-feedforward.gv"))
-    # visualize.draw_net(config, winner, view=True, node_names=node_names,
-    #                    filename="winner-feedforward-enabled-pruned.gv", prune_unused=True)
+    visualize.draw_net(config, winner, view=True, node_names=node_names,
+                       filename=os.path.join(local_dir, "winner-feedforward.gv"))
 
 
 if __name__ == '__main__':
