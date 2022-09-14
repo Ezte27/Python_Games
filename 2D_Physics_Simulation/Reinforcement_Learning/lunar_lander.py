b7d79a35c1a68391679c0a4c3c6207676b70467c
Esteban <estedcg27@gmail.com>
2022-09-14 12:14:49 -0400

    a

---
+++ 2D_Physics_Simulation/Reinforcement_Learning/lunar_lander.py
@@ -0,0 +1,19 @@
+import gym
+
+env = gym.make("LunarLander-v3", render_mode="human")
+
+observation = env.reset()
+
+print(observation)
+print(env.action_space)
+
+terminated = False
+truncated = False
+for _ in range(1000):
+    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+    print(env.action_space.sample())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
