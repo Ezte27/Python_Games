d704d765048e88412e0a6ce4990be6a16591987c
Esteban <estedcg27@gmail.com>
2022-09-14 12:25:51 -0400

    a

--- 2D_Physics_Simulation/Reinforcement_Learning/lunar_lander.py
+++ 2D_Physics_Simulation/Reinforcement_Learning/lunar_lander.py
@@ -1,17 +1,22 @@
 import gym
+from pygame.time import Clock
 
-env = gym.make("LunarLander-v3", render_mode="human")
+env = gym.make("LunarLander-v2", render_mode="human")
 
 observation = env.reset()
 
 print(observation)
 print(env.action_space)
 
+clock = Clock()
+FPS = 50
+
 terminated = False
 truncated = False
 for _ in range(1000):
+    clock.tick(FPS)
+
     observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-    print(env.action_space.sample())
 
     if terminated or truncated:
         observation, info = env.reset()
