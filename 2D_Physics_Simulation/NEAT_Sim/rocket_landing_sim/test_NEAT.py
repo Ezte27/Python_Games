8a1c9fcc669dc09c37379a3b4f9b1f62540372ef
Esteban <estedcg27@gmail.com>
2022-09-18 01:55:17 -0400

    not learning !!!!

--- 2D_Physics_Simulation/NEAT_Sim/rocket_landing_sim/test_NEAT.py
+++ 2D_Physics_Simulation/NEAT_Sim/rocket_landing_sim/test_NEAT.py
@@ -4,6 +4,7 @@ from colorama import Fore
 import pickle
 import neat
 import gym
+import pygame
 import numpy as np
 import matplotlib.pyplot as plt
 import rocket_lander_gym
@@ -61,40 +62,58 @@ def display_stats(steps:int, fitness_per_step: list, view:bool = True, ylog:bool
 
     plt.close()
 
+def render_font(font, text, display):
+    text_surf = font.render(text, False, 'Black')
+    text_rect = text_surf.get_rect(topleft = (10, 10))
+    display.blit(text_surf, text_rect)
+    pygame.display.update()
+
 env = gym.make("RocketLander-v0", render_mode="human")
-observation, info = env.reset()
+observation, info = env.reset(seed=2)
+
+display_surface = pygame.display.get_surface()
+font = pygame.font.SysFont('ariel', 20)
 
 terminated = False
 truncated = False
 
 runs = 0
-MAX_RUNS = 10
+MAX_RUNS = 2
 
 steps = 0
 total_reward = 0
 reward_per_step = []
+total_reward_per_step = []
 while runs < MAX_RUNS:
+    steps += 1
+
     try:
         action = np.argmax(net.activate(observation))
+
     except NameError:
         action = env.action_space.sample()
-    #action = 0 if steps < 60 else 2
 
-    # if action == 2 and steps > 80:
+    #action = 6 if action == 2 and steps < 200 else action
+
+    # if 250 < steps < 300:
     #     time.sleep(1)
 
     observation, reward, terminated, truncated, info = env.step(action)
     
-    steps += 1
+    print(reward)
+
     total_reward += reward
     reward_per_step.append(reward)
-    print(observation)
+    total_reward_per_step.append(total_reward)
+    
+    render_font(font, str(steps), display_surface)
 
     if terminated or truncated:
         runs += 1
-        observation, info = env.reset()
+        observation, info = env.reset(seed=2)
 
 print(f"step: {steps}, total_reward: {total_reward}")
-display_stats(steps, reward_per_step, filename = os.path.join(local_dir, 'stats/fitness.png'))
+display_stats(steps, reward_per_step, filename = os.path.join(local_dir, 'stats/fitness1.png'))
+display_stats(steps, total_reward_per_step, filename = os.path.join(local_dir, 'stats/fitness2.png'))
 
 env.close()
--- 2D_Physics_Simulation/NEAT_Sim/rocket_landing_sim/train_NEAT.py
+++ 2D_Physics_Simulation/NEAT_Sim/rocket_landing_sim/train_NEAT.py
@@ -61,7 +61,7 @@ def run():
                          config_path)
 
     pop = neat.Population(config)
-    #pop = neat.Checkpointer.restore_checkpoint(os.path.join(local_dir, 'neat-checkpoint-460'))
+    #pop = neat.Checkpointer.restore_checkpoint(os.path.join(local_dir, 'neat-checkpoint-82'))
     
     stats = neat.StatisticsReporter()
     pop.add_reporter(stats)
@@ -69,7 +69,7 @@ def run():
     pop.add_reporter(neat.Checkpointer(25))
 
     pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), eval_genome)
-    winner = pop.run(pe.evaluate, 450)
+    winner = pop.run(pe.evaluate, 100)
 
     # Save the winner.
     with open(os.path.join(local_dir, 'stats/winner.pickle'), 'wb') as f:
