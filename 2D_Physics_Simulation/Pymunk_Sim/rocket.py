89ba20bf55f261ca9b9f34efcad5638731e9947d
Esteban <estedcg27@gmail.com>
2022-09-25 19:46:12 -0400

    Alpha 0.3

--- 2D_Physics_Simulation/Pymunk_Sim/rocket.py
+++ 2D_Physics_Simulation/Pymunk_Sim/rocket.py
@@ -10,17 +10,17 @@ pygame.init()
 
 # Display Setup
 
-VIEWPORT_WIDTH         = 1000
-VIEWPORT_HEIGHT        = 600
+VIEWPORT_WIDTH         = 1200
+VIEWPORT_HEIGHT        = 1012
 WINDOW                 = pygame.display.set_mode((VIEWPORT_WIDTH, VIEWPORT_HEIGHT))
+CLOCK                  = pygame.time.Clock()
 
-FPS                    = 60
-SCALE                  = 0.35  # Temporal Scaling, lower is faster - adjust forces appropriately
+FPS                    = 120
+SCALE                  = 0.5  # Temporal Scaling, lower is faster - adjust forces appropriately
 
 # Pymunk Space Setup
-
-X_GRAVITY, Y_GRAVITY   = (0, int(956 * SCALE))
-STARTING_POS           = (VIEWPORT_WIDTH//2, VIEWPORT_HEIGHT - 200)
+X_GRAVITY, Y_GRAVITY   = (0, 956 )#* SCALE)
+STARTING_POS           = (VIEWPORT_WIDTH//2, -200)
 
 # Sky
 SKY_COLOR              = (126, 150, 233)
@@ -31,10 +31,13 @@ GIMBAL_THRESHOLD       = 0.4
 MAIN_ENGINE_POWER      = 16000 * SCALE
 SIDE_ENGINE_POWER      = 1200 / FPS * SCALE
 
-ROCKET_WIDTH           = 6.5 * SCALE
-ROCKET_HEIGHT          = ROCKET_WIDTH / 3.7 * 47.9
-ENGINE_HEIGHT          = ROCKET_WIDTH * 0.5
-ENGINE_WIDTH           = ENGINE_HEIGHT * 0.7
+ROCKET_WIDTH           = 40 * SCALE
+ROCKET_HEIGHT          = ROCKET_WIDTH * 5
+ROCKET_SIZE = (ROCKET_WIDTH, ROCKET_HEIGHT)
+ROCKET_MASS = 30 * SCALE
+
+# ENGINE_HEIGHT          = ROCKET_WIDTH * 0.5
+# ENGINE_WIDTH           = ENGINE_HEIGHT * 0.7
 
 FIRE_WIDTH             = ROCKET_WIDTH * 3
 FIRE_HEIGHT            = FIRE_WIDTH * 3.4
@@ -43,27 +46,41 @@ COLD_GAS_WIDTH         = FIRE_WIDTH/1.4
 COLD_GAS_HEIGHT        = COLD_GAS_WIDTH * 3
 THRUSTER_HEIGHT        = ROCKET_HEIGHT * 0.86
 
-ROCKET_ELASTICITY      = 0.4
+ROCKET_ELASTICITY      = 0.1
 ROCKET_FRICTION        = 0.5
-ROCKET_COLOR           = (20, 20, 20, 97)
+ROCKET_COLOR           = (81, 79, 79, 250)
 
 # LEGS
-LEG_LENGTH             = ROCKET_WIDTH * 2.2
-BASE_ANGLE             = -0.27
-SPRING_ANGLE           = 0.27
-LEG_AWAY               = ROCKET_WIDTH / 2
+LEG_HEIGHT             = ROCKET_SIZE[1] * 0.35
+LEG_SPRING_HEIGHT      = ROCKET_SIZE[1] * 0.1
+LEG_SIZE               = (ROCKET_SIZE[0] * 0.3, ROCKET_SIZE[1] * 0.4)
+LEG_MASS               = 8 * SCALE
+LEG_COLOR              = (220, 20, 30, 20)
+LEG_ELASTICITY         = 0.3
+LEG_FRICTION           = 0.6
+
+# LEG_LENGTH             = ROCKET_WIDTH * 3
+# LEG_WIDTH              = ROCKET_WIDTH * 0.8
+# BASE_ANGLE             = -0.27
+# SPRING_ANGLE           = 0.27
+# LEG_AWAY               = ROCKET_WIDTH / 2
 
 # LANDING PAD
-LANDING_PAD_HEIGHT     = ROCKET_WIDTH
-LANDING_PAD_WIDTH      = LANDING_PAD_HEIGHT * 40
+LANDING_PAD_HEIGHT     = ROCKET_WIDTH * 0.6
+LANDING_PAD_WIDTH      = LANDING_PAD_HEIGHT * 18
+LANDING_PAD_SIZE       = (LANDING_PAD_WIDTH, LANDING_PAD_HEIGHT)
 
 LANDING_PAD_ELASTICITY = 0.3
-LANDING_PAD_FRICTION   = 0.6
+LANDING_PAD_FRICTION   = 0.7
 LANDING_PAD_COLOR      = (220, 234, 233, 97)
 
 # SMOKE FOR VISUALS
 SMOKE_LIFETIME         = (FPS, 2 * FPS) # Smoke Lifetime ranges from 1*fps to 2*fps
 MAX_PARTICLES          = 8
+
+# WATER
+WATER_HEIGHT           = 80 * SCALE
+WATER_COLOR            = (0, 157, 196, 180)
 
 # OTHER
 DRAW_FLAGS             = True
@@ -78,8 +95,6 @@ class Rocket:
     def __init__(
         self, 
         space:       Optional[pymunk.Space] = None,
-        body:        Optional[pymunk.Body]  = None, 
-        shape:       Optional[pymunk.Shape] = None,
         render_mode: Optional[str]          = None,
         gravity:     tuple                  = (0, 20),
         clock:       Optional[bool]         = False
@@ -120,14 +135,8 @@ class Rocket:
         self.render_mode = render_mode
 
         self.isopen      = True
-
-        if body is not None and shape is not None:
-            self.body, self.shape = body, shape
         
-        else:
-            self._setup()
-
-        self.position  = pygame.math.Vector2((self.body.position))
+        self._setup()
 
         self.throttle  = 0
         self.gimbal    = 0.0
@@ -140,26 +149,93 @@ class Rocket:
         self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
     
     def _setup(self,):
-        size = (ROCKET_WIDTH, ROCKET_HEIGHT)
-        pos =  STARTING_POS
 
-        self.body = pymunk.Body(body_type = pymunk.Body.DYNAMIC)
-        self.body.position = pos
+        self._create_water()
+        
+        self.lander       = self._create_lander()
 
-        self.shape = pymunk.Poly.create_box(self.body, size)
-        self.shape.elasticity = ROCKET_ELASTICITY
-        self.shape.friction = ROCKET_FRICTION
-        self.shape.color = ROCKET_COLOR
-        self.space.add(self.body, self.shape)
+        self.legs         = []
+        self.leg_contacts = []
+        self._create_legs(self.lander.body)
+
+        self.landing_pad  = self._create_landing_pad()
+    
+    def _create_lander(self):
+        size             = ROCKET_SIZE
+        pos              = STARTING_POS
+
+        body             = pymunk.Body(body_type = pymunk.Body.DYNAMIC)
+        body.position    = pos
+
+        shape            = pymunk.Poly.create_box(body, size)
+        shape.mass       = ROCKET_MASS
+        shape.elasticity = ROCKET_ELASTICITY
+        shape.friction   = ROCKET_FRICTION
+        shape.color      = ROCKET_COLOR
+        self.space.add(body, shape)
+
+        return shape
+    
+    def _create_legs(self, rocket: pymunk.Body):
 
         leg_rects = [
-            [],
-            []
+            [LEG_SIZE, (rocket.position[0] - ROCKET_SIZE[0]//2 - LEG_SIZE[0]//2, rocket.position[1] + LEG_HEIGHT + LEG_SIZE[1]/2), -1],
+            [LEG_SIZE, (rocket.position[0] + ROCKET_SIZE[0]//2 + LEG_SIZE[0]//2, rocket.position[1] + LEG_HEIGHT + LEG_SIZE[1]/2), 1]
         ]
 
-        for leg in leg_rects:
-            for pos, size in leg:
-                pass
+        for size, pos, leg_side in leg_rects:
+            body             = pymunk.Body(body_type = pymunk.Body.DYNAMIC)
+            body.position    = pos
+
+            shape            = pymunk.Poly.create_box(body, size)
+            shape.mass       = LEG_MASS
+            shape.elasticity = LEG_ELASTICITY
+            shape.friction   = LEG_FRICTION
+            shape.color      = LEG_COLOR
+
+            # A PinJoint connecting the leg and the rocket
+            pinjoint = pymunk.PinJoint(body, rocket, (-leg_side * LEG_SIZE[0]/2, -LEG_SIZE[1] / 2), (leg_side * ROCKET_SIZE[0]/2, LEG_HEIGHT))
+
+            # A SlideJoint connecting the leg and the rocket
+            slidejoint = pymunk.SlideJoint(body, rocket, (leg_side * LEG_SIZE[0]/2, LEG_SIZE[1] / 2.6), (leg_side * ROCKET_SIZE[0]/2, LEG_SPRING_HEIGHT), 100 * SCALE, 110 * SCALE)
+            slidejoint.max_force = 50000 * SCALE * 2
+
+            # A SpringJoint connecting the leg and the rocket
+            springjoint = pymunk.DampedSpring(body, rocket, (leg_side * LEG_SIZE[0]/2, LEG_SIZE[1] / 2.6), (leg_side * ROCKET_SIZE[0]/2, LEG_SPRING_HEIGHT), -20000 * SCALE, -2.5, 10)
+            springjoint.max_force = 5000 * SCALE * 2
+
+            self.legs.append(shape)
+
+            self.space.add(body, shape, pinjoint, slidejoint, springjoint)
+        
+        # A SpringJoint connecting both legs
+        springjoint = pymunk.DampedSpring(self.legs[0].body, self.legs[1].body, (LEG_SIZE[0]/2, LEG_SIZE[1] / 3), (-LEG_SIZE[0]/2, LEG_SIZE[1] / 3), -2000 * SCALE, -2.5, 10)
+        springjoint.max_force = 8000 * SCALE * 2
+        self.space.add(springjoint)
+    
+    def _create_water(self):
+        water_body          = pymunk.Body(body_type=pymunk.Body.STATIC)
+        water_body.position = (VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT - (WATER_HEIGHT / 2))
+
+        water               = pymunk.Poly.create_box(water_body, (VIEWPORT_WIDTH, WATER_HEIGHT))
+        water.friction      = 3
+        water.elasticity    = 0.2
+        water.color         = WATER_COLOR
+        self.space.add(water_body, water)
+
+    def _create_landing_pad(self):
+        size             = LANDING_PAD_SIZE
+        pos              = (VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT - (WATER_HEIGHT) - (LANDING_PAD_SIZE[1] / 2))
+
+        body             = pymunk.Body(body_type = pymunk.Body.STATIC)
+        body.position    = pos
+        shape            = pymunk.Poly.create_box(body, size)
+        shape.elasticity = LANDING_PAD_ELASTICITY
+        shape.friction   = LANDING_PAD_FRICTION
+        shape.color      = LANDING_PAD_COLOR
+        self.space.add(body, shape)
+
+        return shape
     
     def _create_particle(self, mass, x, y, ttl, radius):
         p = None
@@ -185,43 +261,69 @@ class Rocket:
         while self.particles and (clean_all or self.particles[0].ttl < 0):
             self.world.DestroyBody(self.particles.pop(0))
     
-    def _create_landing_pad(self):
-        rect = [
-            (VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT - LANDING_PAD_HEIGHT),
-            (LANDING_PAD_WIDTH, LANDING_PAD_HEIGHT)
-        ]
+    def _check_leg_contacts(self, check_landing_pad):
+        contacts = []
+        shape = self.landing_pad if check_landing_pad else self.water
 
-        for pos, size in rect:
-            body = pymunk.Body(body_type = pymunk.Body.STATIC)
-            body.position = pos
-            shape = pymunk.Poly.create_box(body, size)
-            shape.elasticity = LANDING_PAD_ELASTICITY
-            shape.friction   = LANDING_PAD_FRICTION
-            shape.color      = LANDING_PAD_COLOR
-            self.space.add(body, shape)
+        if len(self.legs) > 0 and ((self.landing_pad is not None and check_landing_pad is True) or (self.water is not None and check_landing_pad is not True)):
+            for leg in self.legs:
+                contacts.append((leg.shapes_collide(shape).normal)[1] >= 1)
+
+        else:
+            return [False, False]
+        
+        return contacts
     
     def _destroy(self):
-        if not self.water:
-            return
-        self.world.contactListener = None
-        self.world.DestroyBody(self.water)
-        self.water = None
-        self.world.DestroyBody(self.lander)
+        self.space = None
+
         self.lander = None
-        self.world.DestroyBody(self.ship)
-        self.ship = None
-        self.world.DestroyBody(self.legs[0])
-        self.world.DestroyBody(self.legs[1])
-        self.legs = []
-        self.world.DestroyBody(self.containers[0])
-        self.world.DestroyBody(self.containers[1])
-        self.containers = []
-        self.particles = []
+        self.water  = None
+        self.landing_pad = None
+        self.legs   = []
+        self.leg_contacts = []
+
+        self.screen  = None
+
+        self.dt          = 0
+
+        self.throttle  = 0
+        self.gimbal    = 0.0
+        self.power     = 0
+        self.angle     = 0.0
+        self.force_dir = 0
+
         self.draw_options = None
 
-    def reset(self):
-        self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
         self._clean_particles(True)
+
+    def reset(self, seed:int = None):
+        self._destroy()
+        
+        self.space = pymunk.Space()
+
+        self.space.gravity = (X_GRAVITY, Y_GRAVITY)
+
+        self.screen  = pygame.display.get_surface()
+        
+        if self.clock:
+            self.clock = pygame.time.Clock()
+        else:
+            self.clock = None
+
+        self.dt          = 1 / self.metadata['render_fps']
+        self.render_mode = self.render_mode
+
+        self.isopen      = True
+        
+        self._setup()
+
+        self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
+        
+        # Apply random angular vel to the rocket
+        if seed:
+            np.random.seed(seed)
+        self.lander.body.apply_impulse_at_local_point((np.random.randint(-100 * SCALE, 100 * SCALE, 1), 0), (0, -ROCKET_SIZE[1]/2))
     
     def step(self, action):
 
@@ -245,16 +347,19 @@ class Rocket:
         self.power = 0 if self.throttle == 0.0 else (MIN_THROTTLE + self.throttle * (1 - MIN_THROTTLE)) * (SCALE * 2)
 
         # main engine force
-        force_pos = (self.position.x, self.position.y)
+        force_pos = (self.lander.body.position[0], self.lander.body.position[1])
         force = (-np.sin(self.angle + self.gimbal) * MAIN_ENGINE_POWER * self.power,
                  np.cos(self.angle + self.gimbal) * MAIN_ENGINE_POWER * self.power)
-        self.lander.ApplyForce(force=force, point=force_pos, wake=False)
+        self.lander.body.apply_force_at_local_point(force=force, point=force_pos)
         
         # control thruster force
         # force_pos_c = self.position + THRUSTER_HEIGHT * np.array(
         #               (np.sin(self.angle), np.cos(self.angle)))
         # force_c = (-self.force_dir * np.cos(self.angle) * SIDE_ENGINE_POWER,
         #            self.force_dir * np.sin(self.angle) * SIDE_ENGINE_POWER)
+
+        # Checking for leg contact with landing pad
+        self.leg_contacts = self._check_leg_contacts(True)
 
         self.space.step(self.dt)
 
@@ -361,9 +466,9 @@ class Rocket:
             if len(self.particles) <= MAX_PARTICLES:
                 if self.particles:
                     if self.particles[-1].ttl < 0.6:
-                        self._create_particle(0.02, randrange(-10, 10) + self.lander.position[0], randrange(-10, 10) + self.lander.position[1] - ENGINE_HEIGHT, 1.0, 45 * SCALE)
+                        self._create_particle(0.02, randrange(-10, 10) + self.lander.position[0], randrange(-10, 10) + self.lander.position[1] - ROCKET_HEIGHT, 1.0, 45 * SCALE)
                 else:
-                    self._create_particle(0.02, randrange(-10, 10) + self.lander.position[0], randrange(-10, 10) + self.lander.position[1] - ENGINE_HEIGHT, 1.0, 45 * SCALE)
+                    self._create_particle(0.02, randrange(-10, 10) + self.lander.position[0], randrange(-10, 10) + self.lander.position[1] - ROCKET_HEIGHT, 1.0, 45 * SCALE)
 
         self._clean_particles(False)
 
@@ -372,16 +477,16 @@ class Rocket:
         if self.render_mode == "human":
             assert self.screen is not None, "Screen is NONE"
 
-            self.space.debug_draw(self.draw_options)
-
             self.screen.blit(self.surf, (0, 0))
+
+            self.space.debug_draw(self.draw_options)
 
             pygame.event.pump()
 
             if self.clock is not None:
                 self.clock.tick(self.metadata["render_fps"])
 
-            pygame.display.update()
+            pygame.display.flip()
 
         elif self.render_mode == "rgb_array":
             return np.transpose(
@@ -426,34 +531,6 @@ def create_boundaries(space, width, height):
         shape.color = (20, 20, 20, 97)
         space.add(body, shape)
 
-def create_pendulums(space, width, height):
-    rad = 25
-
-    rects = [
-        [(width//2 - rad*4, 25), rad, 100],
-        [(width//2 - rad*2, 25), rad, 100],
-        [(width//2, 25), rad, 100],
-        [(width//2 + rad*2, 25), rad, 100],
-        [(width//2 + rad*4, 25), rad, 100]
-    ]
-
-    for pos, radius, mass in rects:
-        rotation_center_body = pymunk.Body(body_type = pymunk.Body.STATIC)
-        rotation_center_body.position = pos
-
-        body = pymunk.Body(body_type=pymunk.Body.DYNAMIC)
-        body.position = (pos[0], pos[1] + 200)
-
-        circle = pymunk.Circle(body, radius)
-        
-        rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0, 0), (0, 0))
-
-        circle.friction = 0.01
-        circle.mass = mass
-        circle.elasticity = 1
-
-        space.add(body, circle, rotation_center_joint)
-
 def create_circle(space, radius, mass, pos):
     body = pymunk.Body(body_type = pymunk.Body.STATIC)
     body.position = (pos)
@@ -466,17 +543,13 @@ def create_circle(space, radius, mass, pos):
     return shape
 
 def run():
-    
-    window = pygame.display.set_mode((VIEWPORT_WIDTH, VIEWPORT_HEIGHT))
-    clock = pygame.time.Clock()
-    FPS = 60
 
     #create_boundaries(space, width, height)
-    rocket = Rocket(render_mode = 'human', gravity = (X_GRAVITY, Y_GRAVITY))
+    env = Rocket(render_mode = 'human', gravity = (X_GRAVITY, Y_GRAVITY))
     action = 0
 
-    # pressed_pos = None
-    # ball = None
+    mouse_joint = None
+    mouse_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
 
     run = True
     while run:
@@ -484,6 +557,33 @@ def run():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if mouse_joint is not None:
+                    env.space.remove(mouse_joint)
+                    mouse_joint = None
+
+                p = pymunk.Vec2d(*event.pos)
+                hit = env.space.point_query_nearest(p, 5, pymunk.ShapeFilter())
+                if hit is not None and hit.shape.body.body_type == pymunk.Body.DYNAMIC:
+                    shape = hit.shape
+                    # Use the closest point on the surface if the click is outside
+                    # of the shape.
+                    if hit.distance > 0:
+                        nearest = hit.point
+                    else:
+                        nearest = p
+                    mouse_joint = pymunk.PivotJoint(
+                        mouse_body, shape.body, (0, 0), shape.body.world_to_local(nearest)
+                    )
+                    mouse_joint.max_force = 50000
+                    mouse_joint.error_bias = (1 - 0.15) ** 60
+                    env.space.add(mouse_joint)
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if mouse_joint is not None:
+                    env.space.remove(mouse_joint)
+                    mouse_joint = None
 
         keys = pygame.key.get_pressed()
 
@@ -496,40 +596,24 @@ def run():
         
         if keys[pygame.K_a]:
             action =  0
+            env.reset(seed = 19)
         
         if keys[pygame.K_d]:
             action =  1
 
+        if keys[pygame.K_LCTRL] and keys[pygame.K_s]: # Screenshot
+            pygame.image.save(env.screen, "rocket_landing_sim_screenshot.png")
+
         # Step
-        rocket.step(action)
+        env.step(action)
 
-        # line = None
-        # if ball and pressed_pos:
-        #     line = [pressed_pos, pygame.mouse.get_pos()]
+        # Mouse Interaction
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_body.position = mouse_pos
 
-        # for event in pygame.event.get():
-        #     if event.type == pygame.QUIT:
-        #         run = False
+        CLOCK.tick(FPS)
 
-        #     if event.type == pygame.MOUSEBUTTONDOWN:
-        #         if not ball:
-        #             pressed_pos = pygame.mouse.get_pos()
-        #             ball = create_circle(space, 15, 10, pressed_pos)
-
-        #         elif pressed_pos:
-        #             ball.body.body_type = pymunk.Body.DYNAMIC
-        #             angle = calculate_angle(*line) # * asterisk breaks the list in the different elements
-        #             force = calculate_distance(*line) * 50
-        #             fx = math.cos(angle) * force
-        #             fy = math.sin(angle) * force
-        #             ball.body.apply_impulse_at_local_point((fx, fy), (0, 0))
-        #             pressed_pos = None
-
-        #         else:
-        #             space.remove(ball, ball.body)
-        #             ball = None
-
-        clock.tick(FPS)
+        pygame.display.set_caption(f"fps: {CLOCK.get_fps()}")
 
 run()
 pygame.quit()
