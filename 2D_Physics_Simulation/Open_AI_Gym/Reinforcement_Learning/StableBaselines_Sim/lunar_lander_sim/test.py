a46fc081dca05a5774d7cd1735690991beefc777
Esteban <estedcg27@gmail.com>
2022-10-10 14:10:00 -0400

    update

--- 2D_Physics_Simulation/Open_AI_Gym/Reinforcement_Learning/StableBaselines_Sim/lunar_lander_sim/test.py
+++ 2D_Physics_Simulation/Open_AI_Gym/Reinforcement_Learning/StableBaselines_Sim/lunar_lander_sim/test.py
@@ -1,6 +1,10 @@
 import gym
 from stable_baselines3 import A2C, PPO
+import matplotlib.pyplot as plt
+import warnings
 import os
+import numpy as np
+from colorama import Fore
 
 local_dir   = os.path.dirname(__file__)
 models_dir  = "models"
@@ -13,6 +17,32 @@ env = gym.make("LunarLander-v2")
 observation = env.reset()
 
 model = PPO.load(model_path, env=env)
+
+def display_stats(steps:int, fitness_per_step: list, view:bool = True, ylog:bool = False, filename:str ='stats/fitness.svg'):
+    if plt is None:
+        warnings.warn("This display is unavailable due to missing dependency (matplotlib)")
+    frames = range(steps)
+    fitness = np.array(fitness_per_step)
+
+    plt.plot(frames, fitness, 'b-', label="best")
+
+    plt.title("Genome's fitness during test")
+    plt.xlabel("Steps")
+    plt.ylabel("Fitness")
+    plt.grid()
+    plt.legend(loc="best")
+    if ylog:
+        plt.gca().set_yscale('symlog')
+
+    try:
+        plt.savefig(filename)
+    except FileNotFoundError:
+        print(f"plt.savefig(filename) unavailable because {Fore.RED}FileNotFoundError{Fore.RESET}")
+
+    if view:
+        plt.show()
+
+    plt.close()
 
 for ep in range(EPISODES):
     observation = env.reset()
